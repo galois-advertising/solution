ed662b626781ce47cc5072b421568a2c93a6877c
solopointer <solopointer1202@gmail.com>
2023-09-13 19:56:38 +0800

    upload

--- serialization/serialization.h
+++ serialization/serialization.h
@@ -1,27 +1,102 @@
 #pragma once
+#include <any>
 #include <iostream>
+#include <boost/json.hpp>
 #include <sstream>
-#include "common.h"
-#include "imp.h"
+#include <typeindex>
 
 namespace lixiang_serialization {
+template <typename T>
+using nvp = std::pair<T&, std::string>;
+
+#define _nvp(name) std::make_pair(std::ref(name), std::string(#name))
 
 enum class direction_t : uint8_t {
   serialization, deserialization
 };
 
+typedef boost::json::object json_archive;
+
+template<typename>
+struct is_std_vector : std::false_type {};
+
+template<typename T, typename A>
+struct is_std_vector<std::vector<T,A>> : std::true_type {};
+
+
 class serialization {
  public:
   json_archive &archive;
-
   direction_t direction;
 
   serialization(json_archive &_stream) : archive(_stream) {
   }
 
+  template<typename T>
+  static void dump_to(json_archive &s, nvp<T> &&value) {
+    using TT = typename std::remove_reference<T>::type;
+    if constexpr (std::is_fundamental_v<TT> || std::is_same_v<TT, std::string>) {
+      s[value.second] = value.first;
+    } else if constexpr(is_std_vector<TT>::value) {
+      boost::json::array array;
+      for (auto &item : value.first) {
+        json_archive temp;
+        serialization ser(temp);
+        ser.direction = direction_t::serialization;
+        item.serialization(ser);
+        array.push_back(temp);
+      }
+      s[value.second] = array;
+    } else if constexpr (std::is_same_v<TT, std::any>) {
+      std::any &var = value.first;
+      if (!var.has_value()) {
+        return;
+      }
+      boost::json::object any_value;
+      any_value["typeid"] = std::type_index(var.type()).name();
+      any_value["content"] = "[context of any object]";
+      s[value.second] = any_value;
+    } else {
+      json_archive temp;
+      serialization ser(temp);
+      ser.direction = direction_t::serialization;
+      value.first.serialization(ser);
+      s[value.second] = temp;
+    }
+  }
+
+  template<typename T>
+  static void parse_from(json_archive &s, nvp<T> &&value) {
+    using TT = typename std::remove_reference<T>::type;
+    if (s.contains(value.second.c_str())) {
+      if constexpr (std::is_same_v<TT, int>) {
+        value.first = static_cast<int>(s.at(value.second).as_int64());
+      } else if constexpr (std::is_same_v<TT, std::string>) {
+        value.first = s.at(value.second).as_string().c_str();
+      } else if constexpr(is_std_vector<TT>::value) {
+        value.first.clear();
+        for (auto &item : s.at(value.second).as_array()) {
+          typename TT::value_type temp;
+          serialization ser(item.as_object());
+          ser.direction = direction_t::deserialization;
+          temp.serialization(ser);
+          value.first.push_back(std::move(temp));
+        }
+      } else if constexpr (std::is_same_v<TT, std::any>) {
+        auto item = s.at(value.second).as_object();
+      } else {
+        serialization ser(s.at(value.second).as_object());
+        ser.direction = direction_t::deserialization;
+        value.first.serialization(ser);
+      }
+    } else {
+      throw std::runtime_error("field not exists");
+    }
+  }
+
   template<typename FIRST, typename ...OTHER>
   void serialization_helper(FIRST &&arg1, OTHER&& ...args) {
-    operation_imp<typename std::remove_reference_t<FIRST>::first_type>::dump_to(archive, arg1);
+    dump_to<typename std::remove_reference_t<FIRST>::first_type>(archive, std::forward<FIRST>(arg1));
     if constexpr (sizeof...(args) != 0) {
       serialization_helper(std::forward<OTHER>(args)...);
     }
@@ -29,7 +104,7 @@ class serialization {
 
   template<typename FIRST, typename ...OTHER>
   void deserialization_helper(FIRST &&arg1, OTHER&& ...args) {
-    arg1.first = operation_imp<typename std::remove_reference_t<FIRST>::first_type>::parse_from(archive, arg1.second);
+    parse_from<typename std::remove_reference_t<FIRST>::first_type>(archive, std::forward<FIRST>(arg1));
     if constexpr (sizeof...(args) != 0) {
       deserialization_helper(std::forward<OTHER>(args)...);
     }
--- serialization/test.cpp
+++ serialization/test.cpp
@@ -1,11 +1,7 @@
 #include "serialization.h"
 #include <vector>
 #include <any>
-#include "writer.h"
-#include "stringbuffer.h"
-#include "common.h"
 using namespace lixiang_serialization;
-using namespace rapidjson;
 
 struct Person;
 
@@ -20,16 +16,26 @@ struct Singer {
   }
 };
 
+struct Person {
+  std::string name;
+  int age;
+public:
+  template<typename S>
+  void serialization(S &serializer) {
+    serializer.do_serialization(_nvp(name), _nvp(age));
+  }
+};
 struct Address {
   std::string country;
   std::string city;
   std::string street;
-  std::vector<Person> neighbors;
+  std::vector<Person> neighbor;
+  std::any secret;
  public:
   template<typename S>
   void serialization(S &serializer) {
     serializer.do_serialization(_nvp(country), _nvp(city), _nvp(street));
-    serializer.do_serialization(neighbors);
+    serializer.do_serialization(_nvp(neighbor), _nvp(secret));
   }
 };
 
@@ -43,53 +49,21 @@ struct Friend {
   }
 };
 
-struct Person {
-  std::string name;
-  int age;
-  Address address;
-  std::vector<Friend> _friends;
-  std::any secret;
- public:
-  template<typename S>
-  void serialization(S &serializer) {
-    serializer.do_serialization(_nvp(name), _nvp(age), _nvp(address), _nvp(secret));
-  }
-};
 
 int main() {
-  Singer s{"it is a type str", 12};
-  /*
-  Friend f1{"my best friend", Singer{"rocker", 18}};
-  Friend f2{"new friend", "little girl"};
-  Friend f3{"third friend", 3};
-  Person p2{"p2", 3, Address{"china", "shanghai", "putuo"}};
-  Address addr1{"china", "beijing", "wangjing", {p2}};
-  Person p1{"p1", 4, addr1, {f1, f2, f3}, "the kind!"};
-   */
-
+  Address addr1{"china", "beijing", "wangjing", {{"name1", 12}, {"name2", 13}}, Singer{"aa", 12}};
 
   json_archive data;
-  data.Parse("{}");
   serialization ser(data);
+  // serialization to data
   ser.direction = direction_t::serialization;
-  s.serialization(ser);
-  StringBuffer buffer;
-  Writer<StringBuffer> writer(buffer);
-  data.Accept(writer);
-  // Output {"project":"rapidjson","stars":11}
-  std::cout << buffer.GetString() << std::endl;
-  Singer b;
-  ser.direction = direction_t::deserialization;
-  b.serialization(ser);
-  std::cout << b.type;
+  addr1.serialization(ser);
+  std::cout << data << std::endl;
 
-  // TODO. 以下是伪代码，需要笔试者具体实现 // auto json = dump(p1)
-  // std::cout << json << std::endl // std::cout << p1 << std::endl
-  // auto pp = parse(json);
-  // assert(p1 == pp)
-  // 序列化
-  // 打印序列化结果
-  // 打印 Person 对象 // 反序列化
-  // 反序列化的结果是对的
+  // deserialization from data
+  Address addr2;
+  ser.direction = direction_t::deserialization;
+  addr2.serialization(ser);
+  std::cout << addr2.city;
 }
 
